39f863240ae67da6521628bdf8bb5f3b1358e428
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-02-23 15:41:06 -0500

    save file to database

--- data/create.js
+++ data/create.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const connect = require('./db');
 const Voter = require('./schema');
 
+const fs = require('fs');
+const readline = require('readline');
+const file = readline.createInterface({
+  input: fs.createReadStream('voters.csv')
+});
 connect(); // To the database
 
 // Create some faculty
@@ -12,7 +17,7 @@ connect(); // To the database
 mongoose.connection.dropDatabase()
   //.then(() => harcourt.save())
   //.then(() => torrey.save())
-  //.then(() => lee.save())
+  .then(() => file.save())
   .then(() => mongoose.connection.close())
   .then(() => console.log('Database is ready.'))
   .catch(error => console.error(error.stack));
