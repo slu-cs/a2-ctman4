b66742553f8f59134f5379b044d8ee136c9da6ed
Charlie Tirrell <charlietirrell@MacBook-Pro-5.local>
2020-02-23 20:09:15 -0500

    ss

--- data/query.js
+++ data/query.js
@@ -42,7 +42,7 @@ const queries = [
 // Run the queries in parallel
 Promise.all(queries)
   .then(function(results) {
-    console.log('Registered voters with first name STARR: ', results[0].map(v => v.firstName));
+    console.log('Registered voters with first name STARR: ', results[0]);
     //console.log('Voted in the 2016 general election: ', results[1].map(p => p.history));
     //console.log('Started in 2003: ', results[2].map(p => p.firstName));
     //console.log('Teaches 362: ', results[3].map(p => p.name));
